d29ea58b670e4ca3b2d990ad9e2a2cd0116ccd88
a12345 <102536925+churrasco1@users.noreply.github.com>
2023-07-04 15:20:04 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="CSS/style.css">
+<title>index</title>
+</head>
+
+<body>
+	<section>
+		
+		<header>
+			<a href="index.php"><img src="img/logoazul.png"></a>
+			<div class="navigation">
+				<a href="login_professor.php">Professor</a>
+				<a href="login.php">Admin</a>				
+			</div>		
+		</header>
+	<div class="content">
+		<div class="info">
+			<h2>Menu Principal</h2>
+			<p>Faça login ou registe-se para poder utilizar o site!</p>
+		</div>
+	</div>
+	</section>
+</body>
+</html>
